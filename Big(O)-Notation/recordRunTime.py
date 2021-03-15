3dc1f363c426a264fbfd61db02bd45a4000ae78c
satyamverma95 <satyamverma95@gmail.com>
2021-03-15 16:44:36 +0530

    Update recordRunTime.py
    Adding  O(1) operation time record function, findNBoxes.

--- Big(O)-Notation/recordRunTime.py
+++ Big(O)-Notation/recordRunTime.py
@@ -7,6 +7,12 @@ def findNemo2 ( array ):
       print ( "Found Nemo!")
 
 
+def findNBoxes ( array ):
+  elem_1  = array [0]
+  elem_2  = array [1]
+
+  print ( "Found Elem 1 : {}, Elem 2 : {}".format(elem_1, elem_2))
+
 
 #This signifies the start of the program
 if __name__ == "__main__":
@@ -17,7 +23,18 @@ if __name__ == "__main__":
   
   start =  timeit.default_timer()
   
-  findNemo2( array_large ) #O(n)--> Linear Time. Based on length of array. 
+  findNemo2( array_medium ) #O(n)--> Linear Time. Based on length of array. 
+
+  end   = timeit.default_timer()
+
+  print ( "Run Time : (ms)  ", (end - start) ) 
+
+
+  #Checking time for O(1) operations
+
+  start =  timeit.default_timer()
+  
+  findNBoxes( array_large ) #O(n)--> Linear Time. Based on length of array. 
 
   end   = timeit.default_timer()
 
