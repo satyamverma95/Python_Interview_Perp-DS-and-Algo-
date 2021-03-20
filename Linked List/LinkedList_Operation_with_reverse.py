8716db056a7ac4b6204ad84be7d9ab871647b320
satyamverma95 <satyamverma95@gmail.com>
2021-03-20 16:11:48 +0530

    Create LinkedList_Operation_with_reverse.py
    Linked List operation with reverse function using stacks in Python.

---
+++ Linked List/LinkedList_Operation_with_reverse.py
@@ -0,0 +1,102 @@
+class Node:
+  def __init__( self, data ):
+    self.data = data
+    self.next = None
+
+ 
+
+class LinkedList :
+  def __init__( self ):
+    self.head = None
+
+  def appendToLast ( self, data ):
+
+    if ( not self.head ):
+      self.head = Node( data ) 
+    else:
+      
+      tempIter  = self.head
+      while ( tempIter.next != None ):
+        tempIter  = tempIter.next
+
+      tempIter.next = Node( data )
+
+
+  def addInTheFront ( self, data ):
+
+    if ( not self.head ):
+      self.head = Node ( data )
+    else:
+      newNode = Node( data )
+      newNode.next  = self.head
+      self.head = newNode        
+
+  def insertAfter( self, after, data ):
+    
+    newNode   = Node ( data )
+    tempIter  = self.head
+    
+    while ( tempIter.data != after ):
+      tempIter  = tempIter.next
+
+    newNode.next  = tempIter.next
+    tempIter.next = newNode
+
+
+  def deleteNode ( self, data ):
+
+    if ( not self.head ):
+      return 
+
+    if ( self.head.data == data):
+      self.head = self.head.next
+      return
+
+    tempIter  = self.head
+
+    while ( tempIter.next.data != data ):
+      tempIter  = tempIter.next
+    
+    tempIter.next = tempIter.next.next
+
+  def reverseLL ( self ):
+
+    stack     = list ()
+    tempIter  = self.head
+
+    while ( tempIter.next != None ):
+      stack.append( tempIter.data )
+      prevNode  = tempIter
+      tempIter  = tempIter.next
+      self.deleteNode( prevNode.data )
+
+
+    for item in range( len ( stack ) ):
+      self.appendToLast( stack.pop() )
+
+
+  def printLinkedList ( self ):
+
+    tempIter  = self.head
+
+    print ( "Head ->", end = " " )
+    while ( tempIter != None ):
+      print ( "{} -->".format( tempIter.data ), end=" " )
+      tempIter  = tempIter.next
+    print( "None" )
+
+
+
+if __name__ == "__main__":
+
+  ll       = LinkedList ( )
+
+  ll.addInTheFront(60)
+  ll.appendToLast(56)
+  ll.appendToLast(90)
+  ll.appendToLast(23)
+  ll.insertAfter(56, 78)
+  #ll.deleteNode(  )
+
+  ll.reverseLL()
+  ll.printLinkedList()
